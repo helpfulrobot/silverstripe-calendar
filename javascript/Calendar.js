7779ea8332013f54cf1b1fa9feee09ada1b5be7e
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-09-24 18:13:28 +0000

    ADDING JS previous and next
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/calendar/branches/2.4@5324 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- javascript/Calendar.js
+++ javascript/Calendar.js
@@ -19,7 +19,7 @@ var Calendar = {
 		var prev = jQuery(this.navigationbarID+".previous.ajaxified a");
 		var previousLink  = jQuery(prev).attr("rel");
 		jQuery(prev).attr("href", previousLink);
-		var next = jQuery(jQuery(this.navigationbarID+".next.ajaxified a");
+		var next = jQuery(this.navigationbarID+".next.ajaxified a");
 		var nextLink = jQuery(next).attr("rel");
 		jQuery(next).attr("href", nextLink);
 	}
